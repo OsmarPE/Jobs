d3ad170268d9ce27833b0a440b9e3e77ec36d399
Osmar Perera <osmarperera10@gmail.com>
2025-08-01 01:00:46 -0600

    feat: add module of refister user finish

---
+++ pages/finish-register-user/FinishRegisterSuccess.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FinishRegisterSuccess() {
+    return (
+        <article className="step step--success">
+            <div className="step__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5" /></svg>
+            </div>
+            <h2 className="step__title">Hemos terminado</h2>
+            <p className="step__text">Estas listo para poder encontrar un trabajo, dale a siguiente para regresar al inicio</p>
+            <button className="step__btn btn btn--primary">Empezar</button>
+        </article>
+    )
+}
---
+++ pages/finish-register-user/FinishRegisterUser.tsx
@@ -0,0 +1,126 @@
+'use client';
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import FinishRegisterSuccess from "./FinishRegisterSuccess";
+import Circle from "@/components/landing/Circle";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+export default function FinishRegisterUser() {
+
+    const [section, setSection] = useState('');
+    const [step, setStep] = useState(0);
+    const [success, setSuccess] = useState(false);
+
+
+
+    const formSchema = z.object({
+        location: z.string().min(1, { message: 'Debes ingresar una ubicación' }),
+        salary: z.string().min(1, { message: 'Debes ingresar un salario' })
+    })
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            location: '',
+            salary: ''
+        }
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data);
+    }
+
+    const sections = [
+        {
+            section: 'location',
+            title: 'Hay que asegurarnos de que tus preferencias estén actualizadas. ¿En dónde te encuentras?',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, voluptatibus!'
+        },
+        {
+            section: 'salary',
+            title: '¿Cuál es el salario mínimo que buscas?',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, voluptatibus!'
+        },
+    ]
+
+    const handleNextSection = async(step: number) => {
+        
+        const nextStep = step + 1;
+        
+        if (nextStep === sections.length) {
+            setSuccess(true);
+            return
+        }
+
+        const isValidField = await form.trigger(sections[step].section as any);
+    
+
+        if (!isValidField) {
+            form.handleSubmit(onSubmit)()
+            return
+        }
+
+        form.clearErrors()
+        setStep(nextStep);
+    }
+
+    const handlePreviousSection = () => {
+        setStep(step - 1);
+    }
+
+    return (
+        <Form {...form}>
+            <Circle className="circle-left-header" />
+            {!success ? (<article className="step">
+                <div className="step__bar">
+                    <div className="step__progress"></div>
+                </div>
+                <h2 className="step__title">{sections?.[step]?.title}</h2>
+                <p className="step__text">{sections?.[step]?.content}</p>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="step__form">
+                    {sections?.[step]?.section === 'location' && <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="step__label label">Ubicación</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Mexico" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />}
+                    {
+                        sections?.[step]?.section === 'salary' && <FormField
+                            control={form.control}
+                            name="salary"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="step__label label">Salario</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="$8,000" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    }
+
+                    {step === section.length - 1 ? (
+                        <Input type="submit" value="Siguiente" className="step__submit btn btn--primary" />
+                    ) : (
+                        <button type="button" onClick={ () => handleNextSection(step)} className="step__submit btn btn--primary">Siguiente</button>
+                    )}
+
+                </form>
+            </article>
+            ) : <FinishRegisterSuccess />
+            }
+        </Form>
+    )
+}
